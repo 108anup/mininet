61a48169f300464575b957834772f5c72079ac4d
Anup Agarwal <anupa@andrew.cmu.edu>
2024-11-06 00:22:15 +0000

    Test with vegas

--- examples/parking_lot.py
+++ examples/parking_lot.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+import json
+import os
 import sys
 
 from functools import partial
@@ -14,7 +16,8 @@ from mininet.link import TCLink
 
 flush = sys.stdout.flush
 
-DURATION = 10
+DURATION = 60
+LOGFILE_PATH = '/home/mininet/P/logs/'
 
 
 class ParkingLotTopo(Topo):
@@ -53,26 +56,55 @@ def parking_lot_test(hops: int, bw_mbps: float, delay_ms: float, cca: str):
     )
     net.start()
 
+    senders = [net.get(f'hs{h}') for h in range(hops+1)]
+    receivers = [net.get(f'hr{h}') for h in range(hops+1)]
+
+    info('*** Starting iperf3 test\n')
     for h in range(hops+1):
-        sender = net.get(f'hs{h}')
-        receiver = net.get(f'hr{h}')
+        sender = senders[h]
+        receiver = receivers[h]
         assert isinstance(sender, Node)
         assert isinstance(receiver, Node)
-        logfile = f'/tmp/[s={sender}][r={receiver}].json'
+
+        logfile = os.path.join(LOGFILE_PATH, f'[s={sender}][r={receiver}].json')
+        if os.path.exists(logfile):
+            os.remove(logfile)
+
         receiver.sendCmd(f'iperf3 -s -p 5001 > /dev/null')
         sender.sendCmd(f'iperf3 -c {receiver.IP()} -p 5001 -t {DURATION} --congestion {cca} --json --logfile {logfile}')
 
-    # TODO: log queue buildup
-    CLI(net)
+    for sender in senders:
+        sender.waitOutput()
+    info('*** iperf3 test completed\n')
 
+    throughputs = []
+    for h in range(hops+1):
+        logfile = os.path.join(LOGFILE_PATH, f'[s={senders[h]}][r={receivers[h]}].json')
+        with open(logfile, 'r') as f:
+            data = json.load(f)
+            throughput = float(data["end"]["sum_received"]["bits_per_second"]) / 1e6
+            sender = senders[h]
+            receiver = receivers[h]
+            info(f'*** {sender} -> {receiver} throughput: {throughput:.2f} Mbps\n')
+            throughputs.append(throughput)
+
+    # TODO: log queue buildup
+    # CLI(net)
+    ratio = throughputs[-1]/throughputs[0]
+    info(f"*** {hops}, {ratio}")
     net.stop()
+    return ratio
 
 
 if __name__ == '__main__':
     # TODO: Add queue size
-    hops = 2
+    hops = 3
     bw_mbps = 10
     delay_ms = 10
-    cca = 'reno'
+    cca = 'vegas'
     setLogLevel('info')
-    parking_lot_test(hops, bw_mbps, delay_ms, cca)
+    data = []
+    for hops in [2, 3, 4, 5]:
+        ratio = parking_lot_test(hops, bw_mbps, delay_ms, cca)
+        data.append((hops, ratio))
+    info(data)
