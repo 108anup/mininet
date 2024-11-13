834cd41b7b4b6d9c849d2feb9d5b994dbb4ac55a
Anup Agarwal <anupa@andrew.cmu.edu>
2024-11-13 18:09:06 +0000

    Add fit plotting for htsim outputs

---
+++ analysis/imc_dumbbell.py
@@ -0,0 +1,50 @@
+import scipy.optimize
+import numpy as np
+import pandas as pd
+import os
+from matplotlib import pyplot as plt
+
+
+STORAGE_ROOT = '/home/anupa/Projects/Verification/CCmatic-empirical/experiments/figs/mininet'
+
+
+def func_sqrt(x, a):
+    return a * np.power(x, 2)
+
+
+def func_swift(x, a):
+    return a * np.sqrt(x)
+
+
+def func_vegas(x, a):
+    return a * x
+
+
+FUNC_DICT = {
+    'swift': func_swift,
+    'vegas': func_vegas,
+    'sqrt': func_sqrt,
+}
+
+if __name__ == '__main__':
+    fpath = "/home/anupa/Projects/msr24/uec-experiments/imc/contracts/multiflow/0.01bdp/ssqueue-vs-nflows.csv"
+    df = pd.read_csv(fpath)
+    #   scheme  num_flows  avg_queue_bytes  avg_queue_bdp
+
+    fig, ax = plt.subplots()
+    for scheme, gdf in df.groupby('scheme'):
+        assert isinstance(scheme, str)
+        func = FUNC_DICT[scheme]
+        ret = scipy.optimize.curve_fit(func, gdf['num_flows'], gdf['avg_queue_bdp'])
+        print(scheme, ret)
+
+        ax.plot(gdf['num_flows'], gdf['avg_queue_bdp'], 'X', label=f'{scheme}_data')
+        ax.plot(gdf['num_flows'], func(gdf['num_flows'], *ret[0]), label=f'{scheme}_contract')
+        ax.legend()
+        ax.grid()
+        ax.set_xlabel('Number of Flows')
+        ax.set_ylabel('Queue Size [BDP]')
+        # ax.set_xscale('log')
+        # ax.set_yscale('log')
+        fig.tight_layout()
+        fig.savefig(os.path.join(STORAGE_ROOT, 'fit_dumbbell.svg'))
---
+++ analysis/imc_parking_lot.py
@@ -0,0 +1,52 @@
+import scipy.optimize
+import numpy as np
+import pandas as pd
+import os
+from matplotlib import pyplot as plt
+
+
+STORAGE_ROOT = '/home/anupa/Projects/Verification/CCmatic-empirical/experiments/figs/mininet'
+
+
+def func_sqrt(x, a):
+    return a * np.sqrt(x)
+
+
+def func_swift(x, a):
+    return a * np.power(x, 2)
+
+
+def func_vegas(x, a):
+    return a * x
+
+
+FUNC_DICT = {
+    'swift': func_swift,
+    'vegas': func_vegas,
+    'sqrt': func_sqrt,
+}
+
+if __name__ == '__main__':
+    fpath = "/home/anupa/Projects/msr24/uec-experiments/imc/contracts/parking_lot/first/xratio-vs-hopcount.csv"
+    df = pd.read_csv(fpath)
+    #   scheme  hop_count  throughput_ratio
+    xl = 'hop_count'
+    yl = 'throughput_ratio'
+
+    fig, ax = plt.subplots()
+    for scheme, gdf in df.groupby('scheme'):
+        assert isinstance(scheme, str)
+        func = FUNC_DICT[scheme]
+        ret = scipy.optimize.curve_fit(func, gdf[xl], gdf[yl])
+        print(scheme, ret)
+
+        ax.plot(gdf[xl], gdf[yl], 'X', label=f'{scheme}_data')
+        ax.plot(gdf[xl], func(gdf[xl], *ret[0]), label=f'{scheme}_contract')
+        ax.legend()
+        ax.grid()
+        ax.set_xlabel('Hop count')
+        ax.set_ylabel('Throughput ratio')
+        # ax.set_xscale('log')
+        # ax.set_yscale('log')
+        fig.tight_layout()
+        fig.savefig(os.path.join(STORAGE_ROOT, 'fit_parking_lot.svg'))
